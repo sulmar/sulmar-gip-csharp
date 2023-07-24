a9279bba12f67e637cc7bf5bb198fd9a3383a1eb
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-07-24 12:55:11 +0200

    Utworzenie projektu odczyt z konsoli

---
+++ src/Basics/ConsoleRead/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.Write("Podaj imię: ");
+string firstName = Console.ReadLine();
+
+// TODO: Pobierz i wyświetl drugie imię (secondname)
+
+Console.Write("Podaj drugie imię: ");
+string secondName = Console.ReadLine();
+
+Console.Write("Podaj nazwisko: ");
+string lastName = Console.ReadLine();
+
+string message = $"Hello {firstName} {secondName} {lastName}";
+Console.WriteLine(message);
