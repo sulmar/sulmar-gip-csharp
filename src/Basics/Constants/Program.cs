2f2edf5f18f74b935f719b0e8d4e048d2025bac2
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-07-24 12:54:37 +0200

    Utworzenie projektu Constants

---
+++ src/Basics/Constants/Program.cs
@@ -0,0 +1,16 @@
+﻿// Constants
+
+const float interestRate = 0.3f;
+
+// interestRate = 1;
+
+Console.WriteLine(interestRate);
+
+const byte speedLimit = 140;
+
+if (speed > speedLimit)
+{
+}
+
+
+// TODO: readonly
