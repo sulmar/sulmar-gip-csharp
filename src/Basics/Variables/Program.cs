81ffa7bfda1822fd70cb6a4f462f424d56c1f6d9
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-07-24 12:53:58 +0200

    Utworzenie projektu Variables

---
+++ src/Basics/Variables/Program.cs
@@ -0,0 +1,37 @@
+﻿// Variables
+
+// Statyczne typowanie
+
+string name = "Marcin";
+
+// Notacja Pascal-Case (notacja Pascalowa)
+Console.WriteLine(name);
+
+// Notacja Camel-Case (notacja wielbłądzia)
+string lastName = "Sulecki";
+
+Console.WriteLine(lastName);
+
+// Notacja Snake-Case (notacja wężowa)
+string last_name = "Sulecki";
+
+
+// Konkatenacja
+
+// zła praktyka
+// string fullName = name + " " + lastName;
+
+// Console.WriteLine(name + " " + lastName);
+// Console.WriteLine(fullName);
+
+// Garbage collector
+
+// Interpolacja
+// dobra praktyka
+string fullName = $"{name} {lastName}";
+Console.WriteLine(fullName);
+
+
+
+
+
