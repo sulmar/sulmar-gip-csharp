eed23b5387076297616695ae9660a942f5e09e9f
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-07-25 12:46:02 +0200

    Dodanie pętli do-while

---
+++ src/ControlFlow/DoWhile/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+int i = 10;
+
+do
+{
+    Console.WriteLine("Hello World");
+    i++;
+}
+while (i <= 5);
+
+int id = 0;
+
+do
+{
+    Console.WriteLine("INSERT INTO...");
+    id = 0;
+    Thread.Sleep(1000); 
+
+} while (id == 0);
