027baf1103a6c1be5aab99ed61af78c842a9264b
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-07-25 16:05:57 +0200

    Dodanie listy

---
+++ src/List/Lists/Program.cs
@@ -0,0 +1,47 @@
+﻿
+// string[] members = new string[] { "Marcin", "Agnieszka" };
+
+List<string> members = new List<string>()
+{
+    "Marcin",
+    "Agnieszka"
+};
+
+members.Add("Michał");
+members.Add("MichałH");
+members.Add("Damian");
+
+members[0] = "Bartek";
+
+members.Remove("Damian");
+
+if (!members.Contains("Marcin"))
+{
+    Console.WriteLine("A gdzie Marcin?!");
+}
+
+Display(members);
+
+members.Clear();
+
+
+
+Console.WriteLine("Clear called!");
+
+Display(members);
+
+void Display(List<string> members)
+{
+    foreach (string member in members)
+    {
+        Console.WriteLine(member);
+    }
+}
+
+
+
+
+
+
+
+
