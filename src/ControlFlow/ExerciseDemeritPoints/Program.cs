9595aec7410c8f69787206f39a470d334f6a4abc
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-07-25 16:06:18 +0200

    Refaktoring

---
+++ src/ControlFlow/ExerciseDemeritPoints/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+
+const int speedLimit = 70;
+const int perPoint = 5;
+const int pointsLimit = 12;
+
+while (true)
+{
+    Console.Write("Podaj prędkość: ");
+    string speedString = Console.ReadLine();
+
+    int speed = int.Parse(speedString);
+
+    Check(speed);
+   
+}
+
+void Check(int speed)
+{
+    if (speed <= speedLimit)
+    {
+        Console.WriteLine("OK");
+    }
+    else
+    {
+        int exceedSpeed = speed - speedLimit;
+
+        double points = (double)exceedSpeed / perPoint;
+
+        points = (double)Math.Ceiling(points);
+
+        if (points >= pointsLimit)
+        {
+            Console.WriteLine("License suspended.");
+        }
+        else
+        {
+            Console.WriteLine($"Points {points}");
+        }
+    }
+}
