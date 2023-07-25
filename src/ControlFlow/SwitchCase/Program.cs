51acc4e38b399775432087d588d4ef96a59c9c26
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-07-25 12:47:39 +0200

    Dodanie switch-case

---
+++ src/ControlFlow/SwitchCase/Program.cs
@@ -0,0 +1,44 @@
+﻿// Role
+// If role is guest: Guest User
+// If it is moderator or administrator: Moderator User
+// Otherwise: Unknown User
+
+string role = "a";
+
+// TODO: Zapisz to za pomocą if-else
+
+//if (role == "guest") Console.WriteLine("Guest User");
+//else if (role == "moderator" || role == "administrator") Console.WriteLine("Moderator User");
+//else Console.WriteLine("Unknown User");
+
+switch(role)
+{
+    case "guest":
+        Console.WriteLine("Guest User");
+        break;
+    case "moderator":
+    case "administrator":
+        Console.WriteLine("Moderator User");
+        break;
+    default: Console.WriteLine("Unknown User");
+        break;
+}
+
+int bateryLevel = 20;
+
+switch(bateryLevel)
+{
+    case > 0 and < 30:
+        Console.WriteLine("Low");
+        break;
+    case >= 30 and < 70:
+        Console.WriteLine("Medium");
+        break;
+    default:
+        Console.WriteLine("Hight");
+        break;
+}
+
+// Match Patterns
+
+
