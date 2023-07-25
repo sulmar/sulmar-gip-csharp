f6d5292fbd23cc38f5aec14d3d0936af61056f4b
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-07-25 11:37:38 +0200

    Dodanie operacji arytmetycznych

---
+++ src/Operators/Arithmetic/Program.cs
@@ -0,0 +1,19 @@
+﻿
+int x = 10;
+int y = 3;
+
+Console.WriteLine(x + y);
+Console.WriteLine(x - y);
+Console.WriteLine(x * y);
+Console.WriteLine(x / y);
+Console.WriteLine(x % y); // modulo
+
+// Increment
+Console.WriteLine(++x);  
+Console.WriteLine(x++);
+Console.WriteLine(x);
+
+// Decrement
+Console.WriteLine(--x);
+Console.WriteLine(x--);
+Console.WriteLine(x);
