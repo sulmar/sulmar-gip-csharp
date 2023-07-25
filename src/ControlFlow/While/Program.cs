7d7be66a149f5dd1cce041633c554b87d068c955
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-07-25 12:47:53 +0200

    Dodanie pętli while

---
+++ src/ControlFlow/While/Program.cs
@@ -0,0 +1,28 @@
+﻿
+int i = 1;
+
+/*
+
+    while(condition)
+    {
+      statement
+    }
+
+*/
+
+while (i <= 5)
+{
+    Console.WriteLine("Hello World!");
+    i++;
+}
+
+bool isConnected = true;
+
+Random random = new Random();
+
+while (isConnected)
+{
+    Console.WriteLine(random.Next(20, 100));
+
+    Thread.Sleep(1000);
+}
