7952a1e4ee06a271e4fb7e8aedccdaacac7a1424
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-07-25 12:46:15 +0200

    Dodanie pętli for

---
+++ src/ControlFlow/For/Program.cs
@@ -0,0 +1,25 @@
+﻿
+/*
+ 
+
+ for (init variable; condition; increment/decrement)
+ {
+    statement;
+ }
+
+*/
+
+const int count = 5;
+
+for (int i = 1; i <= count; i++)
+{
+    Console.WriteLine($"Hello, World #{i}!");
+}
+
+for (int i = count; i > 0; i--)
+{
+    Console.WriteLine($"Hello, World #{i}!");
+}
+
+
+
