39f4b0e7d445fc24d419a07938ea76e8c038d536
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-07-25 16:05:37 +0200

    Dodanie struktury

---
+++ src/Basics/Structure/Program.cs
@@ -0,0 +1,34 @@
+﻿
+string street = "Dworcowa";
+string city = "Poznań";
+string country = "Poland";
+string postalCode = "00-000";
+int capacity = 500_000;
+DateOnly buildDate = DateOnly.Parse("1400-01-01");
+
+Address shippingAddress = new Address();
+shippingAddress.Street = street;
+shippingAddress.City = city;
+shippingAddress.Country = country;
+shippingAddress.PostalCode = postalCode;
+shippingAddress.Capacity = capacity;
+shippingAddress.BuildDate = buildDate;
+
+Console.WriteLine($"{shippingAddress.Street} {shippingAddress.City} {shippingAddress.Country}");
+
+
+struct Address
+{
+    public string Street;    
+    public string City;
+    public string Country;
+    public string PostalCode;
+    public int Capacity;
+    public DateOnly BuildDate;
+}
+
+struct Location
+{
+    public double Latitude;
+    public double Longitude;
+}
