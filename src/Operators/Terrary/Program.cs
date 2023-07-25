8a4b517c47b2b36cd585e1d8b7e31d9487ae78ab
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2023-07-25 12:46:53 +0200

    Dodanie operatora Terrary

---
+++ src/Operators/Terrary/Program.cs
@@ -0,0 +1,23 @@
+﻿// If a customer has more than 100 points,
+// they are a 'gold' customer, otherwise,
+// they are a 'silver' customer.
+
+int points = 10;
+
+string customerType = points > 100 ? "gold" : "silver";
+
+// condition ? true statement : false statement
+
+//if (points > 100 )
+//{
+//    customerType = "gold";
+//}
+//else
+//{
+//    customerType = "silver";
+//}
+
+
+
+
+Console.WriteLine(customerType);
